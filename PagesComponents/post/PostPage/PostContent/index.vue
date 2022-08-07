669979b525ec24b05877635d2a9303576ddcd1be
幻彩冰袭 <1994370030@qq.com>
2022-08-07 13:30:58 +0800

    优化post页面的组件拆分逻辑

---
+++ PagesComponents/post/PostPage/PostContent/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="Content">
+        <slot />
+    </div>
+</template>
+
+<style lang="less" scoped>
+.Content {
+    width: 50vw;
+    border: 1px solid yellowgreen;
+}
+</style>
---
+++ PagesComponents/post/PostPage/SideTab/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="SideTab">
+        <slot />
+    </div>
+</template>
+
+<style lang="less" scoped>
+.SideTab {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 30vw;
+    position: relative;
+    // 这里100%的原因是，整个盒子的高度是有左侧子盒子的决定的
+    height: 100%;
+    border: 1px solid yellowgreen;
+}
+</style>
