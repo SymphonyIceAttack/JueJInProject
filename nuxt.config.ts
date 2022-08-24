0a69272ad9dfb69d2e39da9dbd98971aabd6a761
SymphonyIceAttack <1994370030@qq.com>
2022-08-24 13:11:39 +0800

    xx

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 import path from "path";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: ["@pinia/nuxt", "@vueuse/nuxt"],
+    buildModules: ["@pinia/nuxt", "@vueuse/nuxt",['@nuxtjs/dotenv', { path: './' }],],
     types: ["@pinia/nuxt"],
     alias: {
         "@navBarLayout": path.resolve(
